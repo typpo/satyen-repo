a2b31768e01f71c87933577e3735698eb0e74190
Ian <typppo@gmail.com>
2013-07-20 23:57:43 -0700

    handle unicode and add to generated samples

--- generate.py
+++ generate.py
@@ -7,25 +7,35 @@
 
 import sys
 import string
+import unicodedata
 from random import choice, randint
 
 if len(sys.argv) < 2:
   print 'usage: generate <path> <# sample lines>'
   sys.exit(1)
 
+UNICODE_GLYPHS = ''.join(
+    unichr(char)
+    for char in xrange(1114112) # 0x10ffff + 1
+    if unicodedata.category(unichr(char))[0] in ('LMNPSZ')
+    )
+
 # http://docs.oracle.com/javase/specs/jls/se7/html/jls-3.html#jls-3.8
 # Put in some more underscores and periods to test main use case...
 # But I don't think valid java variable names contain periods.
-ALLOWED_CHARS = string.letters + string.digits + '$' \
-    + ('_' * 10) + ('.' * 10)
+ALLOWED_CHARS = unicode(string.letters + string.digits + '$' \
+    + ('_' * 10) + ('.' * 10))
 
 def random_varname(n=20):
-  return ''.join([choice(ALLOWED_CHARS) for x in range(n)])
+  return ''.join([choice(ALLOWED_CHARS) for x in range(n/2)]) \
+        + ''.join([choice(UNICODE_GLYPHS) for x in range(randint(0, 10))]) \
+        + ''.join([choice(ALLOWED_CHARS) for x in range(n/2)])
 
 def generate_file(path, n):
   f = open(path, 'w')
   for i in range(int(n)):
-    f.write('%s,%d\n' % (random_varname(), randint(0, 1e6)))
+    writeme = '%s,%d\n' % (random_varname(), randint(0, 1e6))
+    f.write(writeme.encode('utf8'))
   f.close()
   print 'Done.'
 
