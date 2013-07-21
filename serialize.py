b48be9eaade90b324b71feb510d1b90e91c61a9b
Ian <typppo@gmail.com>
2013-07-20 22:54:19 -0700

    correcting a mistake

--- serialize.py
+++ serialize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Reads CSV of name, score pairs and creates a data structure for quick lookup
-# of names matching "<query>." or "<query>_"
+# of names matching "<query>." or "_<query>."
 #
 # usage:
 # serialize.py <csv_path> <output_path>
@@ -15,27 +15,37 @@ if len(sys.argv) < 2:
 
 f = open(sys.argv[1], 'r')
 
-SPECIAL_CHARS = set(['.', '_'])
 
 lookup_table = {}
 num_entries = 0
 num_total = 0
+
+def put_in_index(lookup_table, index_on, name, score):
+  lookup_table.setdefault(index_on, [])
+  # Some queries can match multiple, so we keep a list in no particular
+  # order..
+  lookup_table[name[:i]].append({ 'score': score, 'name': name })
+
 for line in f.readlines():
   num_total += 1
-  name, idx = line.split(',')
+  name, score = line.split(',')
   has_special_index = False
+  prev_underscore_idx = -1
   for i in range(len(name)):
     c = name[i]
-    if c in SPECIAL_CHARS:
-      # Index the string up to this specific position, because it matches the
-      # desired query format.
-
+    if c == '.':
+      # index <query>.
       index_on = name[:i]
-      lookup_table.setdefault(index_on, [])
-      # Some queries can match multiple, so we keep a list in no particular
-      # order..
-      lookup_table[name[:i]].append({ 'score': int(idx), 'name': name })
+      put_in_index(lookup_table, name[:i], name, int(score))
       num_entries += 1
+
+      # index _<query>. if there was a previous underscore
+      if prev_underscore_idx > -1:
+        put_in_index(lookup_table, name[prev_underscore_idx+1:i], name, int(score))
+        num_entries += 1
+
+    if c == '_':
+      prev_underscore_idx = i
 
   if num_total % 50000 == 0:
     print '%d (%d) ...' % (num_total, num_entries)
